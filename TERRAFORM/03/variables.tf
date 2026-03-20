9de03d872ab3d2549cded9a70c510d41dc9e24fc
george-wro <ugresia@interia.pl>
2026-03-20 14:20:30 +0100

    poprawka wariables

--- TERRAFORM/03/variables.tf
+++ TERRAFORM/03/variables.tf
@@ -17,7 +17,7 @@ variable "project_environment" {
 variable "own_ssh_public_key" {
   description = "This is your own SSH public key - this is not secret, but we want to validate it"
   type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhBERFzW1ZE6fW3PEGOnBI+81s8OfyLroiAOTX01zXE jenkins@uczen-Grzegorz-Borowik"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEsEuUIFF/QePhOMLoDBeGJD/Gd8qNSyTjf14ePidw1Y"
 
   validation {
     condition     = can(regex("^ssh-(rsa|ed25519)\\s+[A-Za-z0-9+/]+={0,3}(\\s+.+)?$", var.own_ssh_public_key))
